14a14a80ecc86370890934ddae191f88997e0d47
SaarangS <69085865+SaarangS@users.noreply.github.com>
2020-09-15 20:09:04 -0500

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -4,17 +4,19 @@ class Block{
            
             restitution :0.4,
             friction :0.0,
-            //isStatic:true
            
         }
+        
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
         World.add(world, this.body);
       }
+    
       display(){
         var angle = this.body.angle;
         var pos= this.body.position;
+          
         push();
         translate(pos.x, pos.y);
         rotate(angle);
